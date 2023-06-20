4849b86f6f9c11edc255bbaaa5861eba39760eb3
emhamza <imumairhamza@gmail.com>
2023-06-20 06:14:25 -0700

    Fix linters

--- student.rb
+++ student.rb
@@ -4,9 +4,6 @@ class Student < Person
   attr_accessor :classroom
 
   def initialize(classroom, age, parent_permission: true, name: 'Unknown')
-    super(age: age, name: name, parent_permission: parent_permission)
-
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
