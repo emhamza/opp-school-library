5f4a78b024be13e231fd2352d1b90a17415963d2
emhamza <imumairhamza@gmail.com>
2023-06-20 00:25:21 -0700

    Mob programming

--- person.rb
+++ person.rb
@@ -1,28 +1,21 @@
-class Person 
-    attr-reader :id, :name, :age
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
 
-    def initialize(id, name, age = "unknown", parent_permision = true)
-        @id = id
-        @name = name
-        @age = age
-        @parent_permision = parent_permision
-    end
-    
-    def name = (new_name)
-        @name = new_name
-    end
+  def initialize(age:, name: 'Unknown', parent_permission: true)
+    # @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def age = (new_age)
-        @age = new_age
-    end
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 
-    def can_use_services?
-        of_age? || @parent_permision
-    end
+  private
 
-    private
-    
-    def of_age?
-        @age >= 18
-    end
+  def of_age?
+    @age >= 18
+  end
 end
