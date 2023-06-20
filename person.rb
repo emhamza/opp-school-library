eacbb69d608b18c59c469eb24c207cc0a833e82c
emhamza <imumairhamza@gmail.com>
2023-06-20 05:42:47 -0700

    Update changes required

--- person.rb
+++ person.rb
@@ -2,8 +2,8 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age:, name: 'Unknown', parent_permission: true)
-    @id = id
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom:, age:, parent_permission: true, name: 'Unknown')
-    super(age: age, name: name, parent_permission: parent_permission)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
