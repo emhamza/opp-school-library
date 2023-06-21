7c2df97d1f5af91a3fbac40f741112519d7a54ea
emhamza <imumairhamza@gmail.com>
2023-06-21 05:33:50 -0700

    Setup associations among classroom, book, rental, and student

--- person.rb
+++ person.rb
@@ -10,6 +10,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
@@ -18,6 +19,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def rentals(book, date)
+    Rental.new(book, date, self)
   end
 
   private
---
+++ rental.rb
@@ -0,0 +1,11 @@
+class Rental
+  attr_accessors :book, :date, :person
+
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    @person = person
+    book.rentals << self
+    person.rentals << self
+  end
+end
