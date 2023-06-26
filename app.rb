a3de7d4d5b0d38dced692ca235308bb108683205
emhamza <imumairhamza@gmail.com>
2023-06-26 04:05:05 -0700

    Refector app.rb

--- app.rb
+++ app.rb
@@ -2,8 +2,11 @@ require_relative 'teacher'
 require_relative 'student'
 require_relative 'book'
 require_relative 'rental'
+require_relative 'manageperson'
 
 class App
+  attr_accessor :books, :people, :rentals
+  
   def initialize
     @people = []
     @books = []
@@ -27,41 +30,41 @@ class App
     end
   end
 
-  def create_teacher
-    print "Teacher's Specialization: "
-    specialization = gets.chomp
-    print "Teacher's age: "
-    age = gets.chomp.to_i
-    print "Teacher's name: "
-    name = gets.chomp
+  # def create_teacher
+  #   print "Teacher's Specialization: "
+  #   specialization = gets.chomp
+  #   print "Teacher's age: "
+  #   age = gets.chomp.to_i
+  #   print "Teacher's name: "
+  #   name = gets.chomp
 
-    teacher = Teacher.new(age, name, specialization, parent_permission: true)
-    @people << teacher
-    puts 'You have successfully registered a Teacher'
-  end
+  #   teacher = Teacher.new(age, name, specialization, parent_permission: true)
+  #   @people << teacher
+  #   puts 'You have successfully registered a Teacher'
+  # end
 
-  def create_student
-    print 'Age: '
-    age = gets.chomp.to_i
-    print 'Name: '
-    name = gets.chomp
-    print 'Has parent permission? [Y/N]: '
+  # def create_student
+  #   print 'Age: '
+  #   age = gets.chomp.to_i
+  #   print 'Name: '
+  #   name = gets.chomp
+  #   print 'Has parent permission? [Y/N]: '
 
-    parent_permission = gets.chomp.downcase
+  #   parent_permission = gets.chomp.downcase
 
-    case parent_permission
-    when 'n'
-      student = Student.new(nil, age, name, parent_permission: false)
-      @people << student
-    when 'y'
-      student = Student.new(nil, age, name, parent_permission: true)
-      @people << student
-    else
-      'You have entered an invalid option'
-    end
+  #   case parent_permission
+  #   when 'n'
+  #     student = Student.new(nil, age, name, parent_permission: false)
+  #     @people << student
+  #   when 'y'
+  #     student = Student.new(nil, age, name, parent_permission: true)
+  #     @people << student
+  #   else
+  #     'You have entered an invalid option'
+  #   end
 
-    puts 'You have successfully registered a Student'
-  end
+  #   puts 'You have successfully registered a Student'
+  # end
 
   def create_person
     print 'Do you want to create a new student (1) or teacher (2)? [input the number]: '
---
+++ manageperson.rb
@@ -0,0 +1,35 @@
+def create_teacher
+    print "Teacher's Specialization: "
+    specialization = gets.chomp
+    print "Teacher's age: "
+    age = gets.chomp.to_i
+    print "Teacher's name: "
+    name = gets.chomp
+
+    teacher = Teacher.new(age, name, specialization, parent_permission: true)
+    @people << teacher
+    puts 'You have successfully registered a Teacher'
+  end
+
+  def create_student
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Has parent permission? [Y/N]: '
+
+    parent_permission = gets.chomp.downcase
+
+    case parent_permission
+    when 'n'
+      student = Student.new(nil, age, name, parent_permission: false)
+      @people << student
+    when 'y'
+      student = Student.new(nil, age, name, parent_permission: true)
+      @people << student
+    else
+      'You have entered an invalid option'
+    end
+
+    puts 'You have successfully registered a Student'
+  end
